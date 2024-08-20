b37cc674acc502ddb5582eec3aa00e58d4eadadc
DinukaBK <59777936+Dinukabk@users.noreply.github.com>
2024-08-20 18:38:17 +0530

    Model

---
+++ GreenHouseBackend/src/main/java/gr/aueb/cf/GreenHouse/model/User.java
@@ -0,0 +1,51 @@
+package gr.aueb.cf.agronitor.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Greenhouse> greenhouseList = new ArrayList<>();
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+//    Convenient Method
+    public void addGreenhouse(Greenhouse greenhouse) {
+        this.greenhouseList.add(greenhouse);
+        greenhouse.setUser(this);
+    }
+}
